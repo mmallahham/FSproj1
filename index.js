d3f03523bd2634ca9a07f61c8c1a0afa30e8b625
mmallahham <mmallahham@hotmail.com>
2018-01-16 02:06:26 +0100

    remove node modules folder from .gitignore

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+
+
+app.route('/')
+  .get(function (req, res) {
+     res.send('welcome to back-end of the Full Stack course / project 1');
+  })
+
 app.route('/questions')
   .get(function (req, res) {
     db.getAllQuestions(function(questions){
