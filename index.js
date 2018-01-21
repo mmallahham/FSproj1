2d906e68a1be12d2cd5a1f8944dd86a4698ce320
mmallahham <mmallahham@hotmail.com>
2018-01-21 05:54:06 +0100

    questions router tested

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var db = require('./src/db');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-
+app.use(bodyParser.json());
 
 
 app.route('/')
   .get(function (req, res) {
-     res.send('welcome to back-end of the Full Stack course / project 1');
+    res.send('welcome to back-end of the Full Stack course / project 1');
   })
 
 app.route('/questions')
@@ -19,18 +20,20 @@ app.route('/questions')
       res.json(questions);
      });
   })
+  .post(function (req, res) {
+    console.log(req.body);
+
+    var id = parseInt(req.params['id']);
+    db.newQuestion(req.body,function(question){
+      res.json(question);
+    });
+  })
+
 
 app.route('/questions/:id')
 .get(function (req, res) {
   var id = parseInt(req.params['id']);
   db.getQuestionByID(id,function(question){
-    console.log(question);
-    res.json(question);
-   });
-})
-.post(function (req, res) {
-  var id = parseInt(req.params['id']);
-  db.newQuestion({'id':id,'name':'nami'},function(question){
     res.json(question);
   });
 })
